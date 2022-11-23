ff719e169b35b993684977440c79c2a015040d1d
Sebastrn <sebastrn@outlook.com>
2022-11-23 10:53:40 -0500

    Initial code push

---
+++ src/main/java/dev/smolinacadena/sebastrnlib/SebastrnLib.java
@@ -0,0 +1,8 @@
+package dev.smolinacadena.sebastrnlib;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(SebastrnLib.ID)
+public final class SebastrnLib {
+    public static final String ID = "sebastrnlib";
+}
